adf9405c4511a61a3e22eac01404d4e6dc7bf037
Ashwaq Aljanahi <94136081+ashwaqaljanahi2021@users.noreply.github.com>
2022-11-10 12:02:43 -0500

    Arrays

---
+++ arrays1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+
+void info()
+{ 
+int x[7] = {4, 5, 13, 16, 17, 18, 67};
+for (int i=0; i<7;i++)
+  cout<<x[i]<<endl;
+
+}
+
+int main()
+{
+info();
+}
